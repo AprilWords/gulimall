e4976191a22460a69d5785d4f3e97de7b872899d
wangjia <wangjiasmile1@163.com>
2020-09-10 18:22:50 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/personal/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
